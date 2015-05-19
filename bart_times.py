0aa92af954c018cae51a44c44f693064e3fc3e06
Daniel Perez-Becker <dperezbecker@yammmer-inc.com>
2015-05-18 22:32:13 -0700

    added bart_times

---
+++ bart_times.py
@@ -0,0 +1,16 @@
+from bart_api import BartApi
+
+bart = BartApi()
+
+data=[]
+
+all_predictions=bart.etd('MCAR')
+
+for i in xrange(len(all_predictions)):
+    times=[]
+    destination=all_predictions[i]['abbreviation']
+    times.append(destination)
+    for j in xrange(len(all_predictions[i]['estimates'])):
+        # print all_predictions[i]['estimates'][j]["minutes"]
+        times.append(all_predictions[i]['estimates'][j]["minutes"])
+    data.append(times)
