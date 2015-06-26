20692512d706211b74d2035bbc95d12a59103f58
Daniel Perez-Becker <dperezbecker@yammmer-inc.com>
2015-06-26 10:59:37 -0700

    added correct error handling for nextbart-led.py

--- nextbart-led.py
+++ nextbart-led.py
@@ -8,39 +8,43 @@ bart = BartApi()
 
 # FIRST ROW IN DISPLAY ROCKRIDGE BART
 
-data=[]
+try:
 
-predictions_ROCK=bart.etd('ROCK')
-for i in xrange(len(predictions_ROCK)):
-    times=[]
-    destination=predictions_ROCK[i]['abbreviation']
-    times.append(destination)
-    for j in xrange(len(predictions_ROCK[i]['estimates'])):
-        # print all_predictions[i]['estimates'][j]["minutes"]
-        times.append(predictions_ROCK[i]['estimates'][j]["minutes"])
-    data.append(times)
+    data=[]
 
-# Train destinations to consider for ROCK: SFIA-y, MLBR-y
-output=[]
+    predictions_ROCK=bart.etd('ROCK')
+    for i in xrange(len(predictions_ROCK)):
+        times=[]
+        destination=predictions_ROCK[i]['abbreviation']
+        times.append(destination)
+        for j in xrange(len(predictions_ROCK[i]['estimates'])):
+            # print all_predictions[i]['estimates'][j]["minutes"]
+            times.append(predictions_ROCK[i]['estimates'][j]["minutes"])
+        data.append(times)
 
-for k in xrange(len(data)):
-    if(data[k][0] == 'SFIA' or data[k][0] == 'MLBR'):
-        color = 'y'
-    else:
-        color = 'not_of_interest'        
-    for l in xrange(len(data[k])-1):
-        if(data[k][l+1] != 'Leaving' and color != 'not_of_interest'):
-            output.append(data[k][l+1].zfill(2)+color)
-        elif(data[k][l+1] == 'Leaving' and color != 'not_of_interest'):
-            output.append('00'+color)
+    # Train destinations to consider for ROCK: SFIA-y, MLBR-y
+    output=[]
 
-top3 = sorted(output)            
-for m in xrange(3):
-    top3.append('  p')
+    for k in xrange(len(data)):
+        if(data[k][0] == 'SFIA' or data[k][0] == 'MLBR'):
+            color = 'y'
+        else:
+            color = 'not_of_interest'        
+        for l in xrange(len(data[k])-1):
+            if(data[k][l+1] != 'Leaving' and color != 'not_of_interest'):
+                output.append(data[k][l+1].zfill(2)+color)
+            elif(data[k][l+1] == 'Leaving' and color != 'not_of_interest'):
+                output.append('00'+color)
+
+    top3 = sorted(output)            
+    for m in xrange(3):
+        top3.append('  p')
 
 
-barttimes=top3[0]+top3[1]+top3[2]
-
+    barttimes=top3[0]+top3[1]+top3[2]
+    
+except:
+    barttimes='  p  p  p'
 
 # END FIRST ROW IN DISPLAY ROCKRIDGE BART
 
@@ -51,40 +55,46 @@ barttimes=top3[0]+top3[1]+top3[2]
 
 # BEGIN SECOND ROW IN DISPLAY DOWNTOWN BERKELEY BART
 
-data=[]
+try:
 
-predictions_DBRK=bart.etd('DBRK')
-for i in xrange(len(predictions_DBRK)):
-    times=[]
-    destination=predictions_DBRK[i]['abbreviation']
-    times.append(destination)
-    for j in xrange(len(predictions_DBRK[i]['estimates'])):
-        # print all_predictions[i]['estimates'][j]["minutes"]
-        times.append(predictions_DBRK[i]['estimates'][j]["minutes"])
-    data.append(times)
+    data=[]
 
-# Train destinations to consider for DBRK: FRMT-y, DALY-r, MLBR-r
-output=[]
+    predictions_DBRK=bart.etd('DBRK')
+    for i in xrange(len(predictions_DBRK)):
+        times=[]
+        destination=predictions_DBRK[i]['abbreviation']
+        times.append(destination)
+        for j in xrange(len(predictions_DBRK[i]['estimates'])):
+            # print all_predictions[i]['estimates'][j]["minutes"]
+            times.append(predictions_DBRK[i]['estimates'][j]["minutes"])
+        data.append(times)
 
-for k in xrange(len(data)):
-    if(data[k][0] == 'FRMT'):
-        color = 'y'
-    elif(data[k][0] == 'DALY' or data[k][0] == 'MLBR'):
-        color = 'r'
-    else:
-        color = 'not_of_interest'        
-    for l in xrange(len(data[k])-1):
-        if(data[k][l+1] != 'Leaving' and color != 'not_of_interest'):
-            output.append(data[k][l+1].zfill(2)+color)
-        elif(data[k][l+1] == 'Leaving' and color != 'not_of_interest'):
-            output.append('00'+color)
+    # Train destinations to consider for DBRK: FRMT-y, DALY-r, MLBR-r
+    output=[]
 
-top3 = sorted(output)            
-for m in xrange(3):
-    top3.append('  p')
-      
+    for k in xrange(len(data)):
+        if(data[k][0] == 'FRMT'):
+            color = 'y'
+        elif(data[k][0] == 'DALY' or data[k][0] == 'MLBR'):
+            color = 'r'
+        else:
+            color = 'not_of_interest'        
+        for l in xrange(len(data[k])-1):
+            if(data[k][l+1] != 'Leaving' and color != 'not_of_interest'):
+                output.append(data[k][l+1].zfill(2)+color)
+            elif(data[k][l+1] == 'Leaving' and color != 'not_of_interest'):
+                output.append('00'+color)
 
-barttimes=barttimes+top3[0]+top3[1]+top3[2]
+    top3 = sorted(output)            
+    for m in xrange(3):
+        top3.append('  p')
+
+
+    barttimes=barttimes+top3[0]+top3[1]+top3[2]
+
+
+except:
+    barttimes=barttimes+'  p  p  p'
 
 # print data
 # print top3
