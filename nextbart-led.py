4873d83a8a78fceda269edeaa331b73afe3d7fe3
Daniel Perez-Becker <dperezbecker@yammmer-inc.com>
2015-06-25 22:21:56 -0700

    flipped bart schedules

--- nextbart-led.py
+++ nextbart-led.py
@@ -6,7 +6,50 @@ localdir="/home/pi/ledbus/"
 
 bart = BartApi()
 
-# FIRST ROW IN DISPLAY DOWNTOWN BERKELEY BART
+# FIRST ROW IN DISPLAY ROCKRIDGE BART
+
+data=[]
+
+predictions_ROCK=bart.etd('ROCK')
+for i in xrange(len(predictions_ROCK)):
+    times=[]
+    destination=predictions_ROCK[i]['abbreviation']
+    times.append(destination)
+    for j in xrange(len(predictions_ROCK[i]['estimates'])):
+        # print all_predictions[i]['estimates'][j]["minutes"]
+        times.append(predictions_ROCK[i]['estimates'][j]["minutes"])
+    data.append(times)
+
+# Train destinations to consider for ROCK: SFIA-y, MLBR-y
+output=[]
+
+for k in xrange(len(data)):
+    if(data[k][0] == 'SFIA' or data[k][0] == 'MLBR'):
+        color = 'y'
+    else:
+        color = 'not_of_interest'        
+    for l in xrange(len(data[k])-1):
+        if(data[k][l+1] != 'Leaving' and color != 'not_of_interest'):
+            output.append(data[k][l+1].zfill(2)+color)
+        elif(data[k][l+1] == 'Leaving' and color != 'not_of_interest'):
+            output.append('00'+color)
+
+top3 = sorted(output)            
+for m in xrange(3):
+    top3.append('  p')
+
+
+barttimes=top3[0]+top3[1]+top3[2]
+
+
+# END FIRST ROW IN DISPLAY ROCKRIDGE BART
+
+# print data
+# print top3
+# print barttimes
+
+
+# BEGIN SECOND ROW IN DISPLAY DOWNTOWN BERKELEY BART
 
 data=[]
 
@@ -38,49 +81,8 @@ for k in xrange(len(data)):
 
 top3 = sorted(output)            
 for m in xrange(3):
-    top3.append('  p')            
-
-barttimes=top3[0]+top3[1]+top3[2]
-
-
-# END FIRST ROW IN DISPLAY DOWNTOWN BERKELEY BART
-
-# print data
-# print top3
-# print barttimes
-
-
-# BEGIN SECOND ROW IN DISPLAY DOWNTOWN BERKELEY BART
-
-data=[]
-
-predictions_ROCK=bart.etd('ROCK')
-for i in xrange(len(predictions_ROCK)):
-    times=[]
-    destination=predictions_ROCK[i]['abbreviation']
-    times.append(destination)
-    for j in xrange(len(predictions_ROCK[i]['estimates'])):
-        # print all_predictions[i]['estimates'][j]["minutes"]
-        times.append(predictions_ROCK[i]['estimates'][j]["minutes"])
-    data.append(times)
-
-# Train destinations to consider for ROCK: SFIA-y, MLBR-y
-output=[]
-
-for k in xrange(len(data)):
-    if(data[k][0] == 'SFIA' or data[k][0] == 'MLBR'):
-        color = 'y'
-    else:
-        color = 'not_of_interest'        
-    for l in xrange(len(data[k])-1):
-        if(data[k][l+1] != 'Leaving' and color != 'not_of_interest'):
-            output.append(data[k][l+1].zfill(2)+color)
-        elif(data[k][l+1] == 'Leaving' and color != 'not_of_interest'):
-            output.append('00'+color)
-
-top3 = sorted(output)            
-for m in xrange(3):
-    top3.append('  p')            
+    top3.append('  p')
+      
 
 barttimes=barttimes+top3[0]+top3[1]+top3[2]
 
